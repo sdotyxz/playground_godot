265c461c95d7706278e71d9e4e3c7c735375f06c
sdotyxz <shengdongtan@gmail.com>
2024-02-26 22:33:56 +0800

    unit animation

---
+++ Game/puzzle_chess/entity/units/unit/unit.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@onready var animation_player = $AnimationPlayer
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 0 #ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_x != 0 || direction_y != 0:
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
+
+	if direction_x != 0:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction_y != 0:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)	
+
+	move_and_slide()
